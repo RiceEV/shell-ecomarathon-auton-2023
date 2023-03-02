3ca76347dc384a169709a1a81ee747d6072d62c4
REV <kevinni20020715@gmail.com>
2023-03-01 20:45:15 -0600

    vehicle control code

--- workspace/competition_code/src/shell_simulation/src/topic_cloner.py
+++ workspace/competition_code/src/shell_simulation/src/topic_cloner.py
@@ -3,6 +3,7 @@
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String, Float64, Bool
+from carla_msgs.msg import CarlaEgoVehicleControl
 from cv_bridge import CvBridge
 import cv2
 
@@ -12,52 +13,60 @@ class TopicCloner(object):
         self.throttle = "/throttle_command"
         self.gear = "/gear_command"
         self.brake = "/brake_command"
-        self.steering = "/steering_command"
-        self.handbrake = "/handbrake_command"
+        self.steer = "/steering_command"
+        self.hand_brake = "/handbrake_command"
+        # tbd: self.reverse = ""?
 
-        self.ego_control = "/carla/ego_vehicle/"
+        self.ego_control = "/carla/ego_vehicle/vehicle_control_cmd"
+        self.loop_rate = rospy.Rate(30)
+        self.ego_sub = rospy.Subscriber("/carla/ego_vehicle/vehicle_control_cmd", CarlaEgoVehicleControl, self.ego_callback)
 
-        # Subscribers
-        rospy.Subscriber(self.throttle, Float64, self.throttle_callback)
-        rospy.Subscriber(self.gear, String, self.gear_callback)
-        rospy.Subscriber(self.brake, Float64, self.brake_callback)
-        rospy.Subscriber(self.steering, Float64, self.steering_callback)
-        rospy.Subscriber(self.handbrake, Bool, self.handbrake_callback)
+        self.throttle_pub = rospy.Publisher(self.throttle, Float64, queue_size = 100)
+        self.gear_pub = rospy.Publisher(self.gear, String, queue_size = 100)
+        self.brake_pub = rospy.Publisher(self.brake, Float64, queue_size = 100)
+        self.steer_pub = rospy.Publisher(self.steer, Float64, queue_size = 100)
+        self.hand_brake_pub = rospy.Publisher(self.hand_brake, Bool, queue_size = 100)
 
-        # Publisher
-        self.pub = rospy.Publisher('chatter', String, queue_size=10)
+    def ego_callback(self, msg):
+        rospy.loginfo("CarlaEgoVehicleControl received...")
 
-    def throttle_callback(self, msg):
-        rospy.loginfo('Throttle received...')
-        self.pub()
+        throttle_msg = Float64()
+        throttle_msg.data = msg.throttle
+        self.throttle_pub.publish(throttle_msg)
 
-    def gear_callback(self, msg):
-        rospy.loginfo('Gear received...')
-        self.image = self.br.imgmsg_to_cv2(msg)
+        gear_msg = String()
+        if msg.gear == 0:
+            gear_msg.data = "forward"
+        else:
+            gear_msg.data = "backward"
 
-    def brake_callback(self, msg):
-        rospy.loginfo('Brake received...')
-        self.image = self.br.imgmsg_to_cv2(msg)
+        self.gear_pub.publish(gear_msg)
 
-    def steering_callback(self, msg):
-        rospy.loginfo('Steering received...')
-        self.image = self.br.imgmsg_to_cv2(msg)
+        brake_msg = Float64()
+        brake_msg.data = msg.brake
+        self.brake_pub.publish(brake_msg)
 
-    def handbrake_callback(self, msg):
-        rospy.loginfo('Handbrake received...')
-        self.image = self.br.imgmsg_to_cv2(msg)
+        steer_msg = Float64()
+        steer_msg.data = msg.steer
+        self.steer_pub.publish(steer_msg)
+
+        hand_brake_msg = Bool()
+        hand_brake_msg.data = msg.hand_brake
+        self.hand_brake_pub.publish(hand_brake_msg)
+
 
     def start(self):
-        rospy.loginfo("Timing images")
+        rospy.loginfo("Timing")
+
         #rospy.spin()
         while not rospy.is_shutdown():
-            rospy.loginfo('publishing image')
+            # rospy.loginfo('publishing')
             #br = CvBridge()
-            if self.image is not None:
-                cv2.imshow('image', self.image)
-                cv2.waitKey(1)
+            # if self.image is not None:
+            #     cv2.imshow('image', self.image)
+            #     cv2.waitKey(1)
             self.loop_rate.sleep()
-        cv2.destroyAllWindows()
+        #cv2.destroyAllWindows()
 
     
 
